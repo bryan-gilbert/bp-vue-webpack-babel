273c15ed0f21556a5c9915d32a01d6e12ac900cb
Bryan Gilbert <bryan.gilbert@memsharp.com>
2018-08-29 12:20:55 -0700

    Rebuild the initial install script

--- README.md
+++ README.md
@@ -20,14 +20,6 @@ npm run serve:production
 ```
 ## Initial install
 
-npm install normalize.css reset-css vue vue-router
-
-
-npm install --save-dev @babel/core @babel/plugin-syntax-dynamic-import @babel/preset-env babel-loader@^8.0.0-beta
-npm install --save-dev cross-env css-loader html-webpack-plugin node-sass sass-loader
-npm install --save-dev vue-loader vue-template-compiler
-npm install --save-dev babel-loader @babel/core @babel/preset-env
-npm install --save-dev webpack webpack-cli webpack-serve
 
 
 ## About
--- install.sh
+++ install.sh
@@ -4,20 +4,22 @@
 #Installs webpack 4.x | babel-loader 8.x | babel 7.x
 npm install --save-dev webpack webpack-cli html-webpack-plugin
 npm install --save-dev babel-loader @babel/core @babel/preset-env @babel/plugin-syntax-dynamic-import
-npm install --save-dev cross-env css-loader node-sass sass-loader
-npm install --save-dev vue-loader
+npm install --save-dev cross-env
+npm install --save-dev vue-loader vue-template-compiler
 
-#
+# CSS SASS
+npm install --save-dev css-loader node-sass sass-loader
+npm install --save-dev optimize-css-assets-webpack-plugin uglifyjs-webpack-plugin
 npm install --save-dev node-sass-magic-importer
-
 # To extract CSS from JS
 npm install --save-dev mini-css-extract-plugin
 
 #webpack development server with hot reloading
 npm install --save-dev webpack-serve opn
 
-# CSS minification
-npm install --save-dev optimize-css-assets-webpack-plugin uglifyjs-webpack-plugin
-
 # production server
 npm install --save-dev express
+
+npm install normalize.css vue vue-router
+#Don't take reset-css v4 yet
+npm install reset-css@^3.0.0
